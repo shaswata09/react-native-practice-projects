a174e89db5ed75abebc0e335f0b3cbbe066426f6
shaswata09 <shaswatamitra@gmail.com>
2020-07-17 19:45:48 +0530

    [Video-10/8] Aded update and delete user item in firebase.

--- rnCli_fourth_app_ecommerce/store/actions/products.js
+++ rnCli_fourth_app_ecommerce/store/actions/products.js
@@ -38,7 +38,16 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-    return { type: DELETE_PRODUCT, pId: productId };
+    return async dispatch => {
+        await fetch(`https://rn-practice-ecommerce.firebaseio.com/products/${productId}.json`, {
+            method: 'DELETE',
+        });
+
+        dispatch({ 
+            type: DELETE_PRODUCT,
+            pId: productId 
+        });
+    };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -73,13 +82,27 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-    return {
-        type: UPDATE_PRODUCT,
-        pId: id,
-        productData: {
-            title,
-            description,
-            imageUrl,
-        }
-    };
+    return async dispatch => {
+        await fetch(`https://rn-practice-ecommerce.firebaseio.com/products/${id}.json`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl,
+            })
+        });
+
+        dispatch({
+            type: UPDATE_PRODUCT,
+            pId: id,
+            productData: {
+                title,
+                description,
+                imageUrl,
+            }
+        });
+    }
 };
